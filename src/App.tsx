bece543e242b05b54990ccb4c8888bab89b18ad4
studiokaiji <studiokaiji@gmail.com>
2023-12-23 20:45:14 +0900

    TopBarを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { TopBar } from "./components/TopBar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <TopBar />
+      </nav>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,9 @@
+import "./TopBar.css";
+
+export const TopBar = () => {
+  return (
+    <div className="top-bar">
+      <h1 className="title-text">🇯🇵 都道府県別総人口推移</h1>
+    </div>
+  );
+};
