a599d71b49360776a87ca2dd39705869e1e4e9cf
studiokaiji <studiokaiji@gmail.com>
2023-12-23 23:01:12 +0900

    テストをsrc内に配置するように変更

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,12 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-import * as path from "path";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
-    include: ["tests/**/*.{js,ts,jsx,tsx}"],
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    include: ["src/**/*.test.{js,ts,jsx,tsx}"],
   },
 });
