52b81acb54feca444eda13b11249a2353cb6b0e9
studiokaiji <studiokaiji@gmail.com>
2023-12-28 16:53:02 +0900

    テストでもaliasを使えるようにした

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     include: ["src/**/*.test.{js,ts,jsx,tsx}"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
   },
 });
