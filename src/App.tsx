c9fae3d5e5c4323f6392812d96e5e05da78ff942
studiokaiji <studiokaiji@gmail.com>
2023-12-29 02:43:01 +0900

    TanStack Queryを追加しswrを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import "./App.css";
-import { TopBar } from "./components/TopBar";
+import "@/App.css";
+import { TopBar } from "@/components/TopBar";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/queryClient";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <TopBar />
-      </nav>
-      <main></main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <nav>
+          <TopBar />
+        </nav>
+        <main></main>
+      </div>
+    </QueryClientProvider>
   );
 }
 
