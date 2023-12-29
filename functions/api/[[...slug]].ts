6542a78cd85c446a2340f3d9fe1636c3abec1e9d
studiokaiji <studiokaiji@gmail.com>
2023-12-29 19:50:54 +0900

    ルーティングを[[...slug]].tsにまとめた

---
+++ functions/api/[[...slug]].ts
@@ -0,0 +1,20 @@
+import { resasFetcher } from "#/services/resasFetcher";
+import { Hono } from "hono";
+import { cache } from "hono/cache";
+
+export const app = new Hono<{ Bindings: Bindigs }>().basePath("/api");
+
+app.get(
+  "*",
+  cache({
+    cacheName: "resas_api_cache",
+    cacheControl: "max-age=2592000",
+  }),
+);
+
+app.get("/prefectures", async (ctx) => {
+  const res = await resasFetcher("/prefectures", ctx.env.RESAS_API_KEY);
+  return res;
+});
+
+export const GET = app.fetch;
