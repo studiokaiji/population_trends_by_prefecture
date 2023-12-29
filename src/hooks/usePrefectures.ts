a887658b931a5610bc8a79a2476c430fa9c32bbf
studiokaiji <studiokaiji@gmail.com>
2023-12-30 04:44:32 +0900

    usePrefecturesを追加

---
+++ src/hooks/usePrefectures.ts
@@ -0,0 +1,21 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const usePrefectures = () => {
+  return useSuspenseQuery({
+    queryKey: ["/api/prefectures"],
+    queryFn: async ({ queryKey }) => {
+      const res = await fetch(queryKey[0]);
+      if (!res.ok) {
+        throw Error(String(res.status));
+      }
+
+      const body = await res.json();
+      if (!("result" in body)) {
+        throw Error("result does not exist");
+      }
+
+      return body.result as PrefecturesResult;
+    },
+    gcTime: 0,
+  });
+};
