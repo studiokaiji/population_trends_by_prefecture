0161d4d26c0d41aacf8fb29f6d32a70c9d6d20a3
studiokaiji <studiokaiji@gmail.com>
2023-12-30 00:30:55 +0900

    kvJsonCacheを利用するようにした

--- functions/api/[[...slug]].ts
+++ functions/api/[[...slug]].ts
@@ -1,20 +1,15 @@
+import { kvJsonCache } from "#/middlewares/kvJsonCache";
 import { resasFetcher } from "#/services/resasFetcher";
 import { Hono } from "hono";
-import { cache } from "hono/cache";
+import { handle } from "hono/cloudflare-pages";
 
 export const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
-app.get(
-  "*",
-  cache({
-    cacheName: "resas_api_cache",
-    cacheControl: "max-age=2592000",
-  }),
-);
+app.use("*", kvJsonCache());
 
 app.get("/prefectures", async (ctx) => {
   const res = await resasFetcher("/prefectures", ctx.env.RESAS_API_KEY);
   return res;
 });
 
-export const GET = app.fetch;
+export const onRequest = handle(app);
