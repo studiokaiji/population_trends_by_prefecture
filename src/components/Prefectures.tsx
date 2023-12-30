efbed880db458afd1d2fda6c0f7f0f5384995a77
studiokaiji <studiokaiji@gmail.com>
2023-12-30 20:49:10 +0900

    Prefecturesにprefectures, onChangePrefecturesを追加

--- src/components/Prefectures.tsx
+++ src/components/Prefectures.tsx
@@ -3,7 +3,15 @@ import { useEffect } from "react";
 import { Checkbox } from "@/components/ui/Checkbox";
 import styles from "@/components/Prefectures.module.css";
 
-export const Prefectures = () => {
+type PrefecturesProps = {
+  prefectures: Prefecture[];
+  onChangePrefectures: (prefectures: Prefecture[]) => void;
+};
+
+export const Prefectures = ({
+  prefectures: selectedPrefectures,
+  onChangePrefectures,
+}: PrefecturesProps) => {
   const { data: prefectures, error: fetchPrefecturesError } = usePrefectures();
 
   useEffect(() => {
@@ -12,12 +20,31 @@ export const Prefectures = () => {
     }
   }, [fetchPrefecturesError]);
 
+  const selectPrefectureHandler = (prefecture: Prefecture) => {
+    const isSelected = selectedPrefectures.some(
+      ({ prefCode }) => prefCode === prefecture.prefCode,
+    );
+
+    if (isSelected) {
+      const filtered = selectedPrefectures.filter(
+        ({ prefCode }) => prefCode !== prefecture.prefCode,
+      );
+      onChangePrefectures(filtered);
+    } else {
+      onChangePrefectures([...selectedPrefectures, prefecture]);
+    }
+  };
+
   return (
     <div className={styles.module}>
       {prefectures?.map((prefecture) => (
         <Checkbox
           key={`prefectures-${prefecture.prefCode}`}
           label={prefecture.prefName}
+          onChange={() => selectPrefectureHandler(prefecture)}
+          checked={selectedPrefectures.some(
+            ({ prefCode }) => prefCode === prefecture.prefCode,
+          )}
         />
       ))}
     </div>
