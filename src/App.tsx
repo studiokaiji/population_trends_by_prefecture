5e11f20d53cce6a6d909dfcb8dcf7dbda1f42ff2
studiokaiji <studiokaiji@gmail.com>
2024-01-01 09:46:57 +0900

    PopulationCompositionChartを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { storagePersister, queryClient } from "@/queryClient";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { Suspense, useState } from "react";
 import { Prefectures } from "./components/Prefectures";
+import { PopulationCompositionChart } from "./components/PopulationCompositionChart";
+import { Select } from "./components/ui/Select";
+import { DATA_TYPES } from "./consts";
 import { Loading } from "./components/ui/Loading";
 import { ScreenCenter } from "./components/layout/ScreenCenter";
 import styles from "@/App.module.css";
 
 function App() {
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+  const [dataType, setDataType] = useState<string>(DATA_TYPES[0]);
+
   return (
     <PersistQueryClientProvider
       client={queryClient}
@@ -32,6 +37,16 @@ function App() {
               onChangePrefectures={setPrefectures}
             />
           </div>
+          <Select
+            options={DATA_TYPES.map((type) => ({
+              value: type,
+            }))}
+            onSelect={setDataType}
+          />
+          <PopulationCompositionChart
+            prefectures={prefectures}
+            dataType={dataType}
+          />
         </div>
       </Suspense>
     </PersistQueryClientProvider>
