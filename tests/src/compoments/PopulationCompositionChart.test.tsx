4a6fa0447eed01e4b1972d7ba0cc20e3d3ae2a05
studiokaiji <studiokaiji@gmail.com>
2024-01-02 07:02:52 +0900

    mockUsePopulationCompositionDataを追加

--- tests/src/compoments/PopulationCompositionChart.test.tsx
+++ tests/src/compoments/PopulationCompositionChart.test.tsx
@@ -20,6 +20,47 @@ globalThis.window.ResizeObserver =
     observe: vi.fn(),
   }));
 
+const mockUsePopulationCompositionData = [
+  {
+    boundaryYear: 2020,
+    data: [
+      {
+        label: "総人口",
+        data: [
+          { year: 2015, value: 1000000 },
+          { year: 2020, value: 990000 },
+        ],
+      },
+      {
+        label: "生産年齢人口",
+        data: [
+          { year: 2015, value: 600000, rate: 54.65 },
+          { year: 2020, value: 550000, rate: 51.13 },
+        ],
+      },
+    ],
+  },
+  {
+    boundaryYear: 2020,
+    data: [
+      {
+        label: "総人口",
+        data: [
+          { year: 2015, value: 500000 },
+          { year: 2020, value: 450000 },
+        ],
+      },
+      {
+        label: "生産年齢人口",
+        data: [
+          { year: 2015, value: 300000, rate: 54.65 },
+          { year: 2020, value: 250000, rate: 51.13 },
+        ],
+      },
+    ],
+  },
+];
+
 const mockData = [
   { year: 2015, 北海道: 1000000, 青森県: 500000 },
   { year: 2020, 北海道: 990000, 青森県: 450000 },
@@ -33,8 +74,8 @@ const mockPrefectures = [
 describe("PopulationCompositionChart", () => {
   it("renders correctly when data is present", () => {
     (usePopulationComposition as Mock).mockReturnValue([
-      { data: mockPrefectures[0], error: null },
-      { data: mockPrefectures[1], error: null },
+      { data: mockUsePopulationCompositionData[0], error: null },
+      { data: mockUsePopulationCompositionData[1], error: null },
     ]);
 
     (convertPopulationComposition as Mock).mockReturnValue(mockData);
@@ -53,7 +94,7 @@ describe("PopulationCompositionChart", () => {
     // Mock the custom hook to return data with error for the first query
     (usePopulationComposition as Mock).mockReturnValue([
       { data: null, error: "An error occurred" },
-      { data: mockPrefectures[1], error: null },
+      { data: mockUsePopulationCompositionData[1], error: null },
     ]);
 
     render(
