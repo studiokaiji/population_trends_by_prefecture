e41a3de79304830dc3992aefa187b100d88eb69f
studiokaiji <studiokaiji@gmail.com>
2024-01-03 03:52:12 +0900

    relative importをaliasを用いたimportに修正

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,9 @@
 import { queryClient, storagePersister } from "@/queryClient";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { type ReactNode, Suspense } from "react";
-import { TopBar } from "./TopBar";
-import { ScreenCenter } from "./layout/ScreenCenter";
-import { Loading } from "./ui/Loading";
+import { TopBar } from "@/components/TopBar";
+import { ScreenCenter } from "@/components/layout/ScreenCenter";
+import { Loading } from "@/components/ui/Loading";
 
 type ContainerProps = {
   children: ReactNode;
